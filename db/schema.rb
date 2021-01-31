d9b2c556c82a1fcd69505863db8e671de772a5d1
DavidNNussbaum <brocknynj@aol.com>
2021-01-31 09:56:54 -0500

    db suceesfully tested in rake console

---
+++ db/schema.rb
@@ -0,0 +1,55 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20210131014316) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text   "note"
+    t.text   "items_to_discuss"
+    t.text   "questions"
+    t.string "patient_id"
+  end
+
+  create_table "histories", force: :cascade do |t|
+    t.text   "diagnoses"
+    t.text   "medications"
+    t.text   "allergies"
+    t.text   "current_treatments"
+    t.text   "surgeries"
+    t.text   "immunizations_with_dates"
+    t.string "patient_id"
+    t.string "subjective_id"
+  end
+
+  create_table "histories_subjectives", force: :cascade do |t|
+    t.string "histories_id"
+    t.string "sbjectives_id"
+    t.string "patient_id"
+  end
+
+  create_table "patients", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
+  end
+
+  create_table "subjectives", force: :cascade do |t|
+    t.text   "location"
+    t.text   "observed_changes"
+    t.text   "sensation_changes"
+    t.string "scale_1_to_10"
+    t.text   "length_of_time"
+    t.string "history_id"
+    t.string "patient_id"
+  end
+
+end
