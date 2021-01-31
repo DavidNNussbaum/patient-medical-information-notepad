ddac1a9072dc67af0a8f899d813f0968f71fc5d6
DavidNNussbaum <brocknynj@aol.com>
2021-01-31 09:11:23 -0500

    seeds.rb has been added

---
+++ db/seeds.rb
@@ -0,0 +1,4 @@
+Patient.new(username: “”, password_digest: “”)
+History.new(diagnoses: “”, medications: “”, allergies: “”, current_treatments: “”, surgeries: “”, immunizations_with_dates: “”)
+Subjective.new(location: “”, observed_changes: “”, sensation_changes: “”, scale_1_to_10: “”, length_of_time: “”)
+Comments.new(note: “”, items_to_discuss: “”, questions:””}
